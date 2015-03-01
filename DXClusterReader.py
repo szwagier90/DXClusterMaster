4ec2ae65d7eb88f7eb6444a0abb8555f995e2a7e
szwagier90 <szwagier90@gmail.com>
2015-03-01 21:32:51 +0100

    Reading spots into the python-list
    Check telnet object for incoming messages every 1 second.

--- DXClusterReader.py
+++ DXClusterReader.py
@@ -8,7 +8,37 @@ print tn.read_some(),
 callsign = "sq6sfs"
 tn.write(callsign + "\n")
 
+#(1)read whole welcome message
 print tn.read_until(">"),
+print tn.read_until("\n")
+#(1)
 
-m = list(tn.read_until("\n"))
-print m
+telnetString = ""
+spots = []
+
+import time
+while True:
+    read = tn.read_eager()
+
+    if read:
+        telnetString += read
+        lines = telnetString.split("\n")
+        if len(lines) > 1:
+            spots.extend(lines[:-1])
+            telnetString = lines[-1]
+
+        for i, spot in enumerate(spots):
+            print i+1,
+            print ": ",
+            print spot
+            
+        print 20*"- ",
+        print len(spots),
+        print 20*" -"
+
+        if telnetString:
+            print telnetString
+
+        print 80*'#'
+    else:
+        time.sleep(1)
