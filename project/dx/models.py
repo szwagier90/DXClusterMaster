803477941ee31a682810c592cbefc86bdaed908f
szwagier89 <szwagier90@gmail.com:wq>
2015-04-23 23:43:21 +0200

    Created a 'Spot' database model

--- project/dx/models.py
+++ project/dx/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Spot(models.Model):
+    spotter = models.CharField(max_length=10)
+    station = models.CharField(max_length=10)
+    frequency = models.FloatField()
+    comment = models.CharField(max_length=40)
+    time = models.DateTimeField()
+    locator = models.CharField(max_length=10)
+
+    def __unicode__():
+        ret = ""
+        ret += spotter + " "
+        ret += station + " "
+        ret += frequency + " "
+        ret += comment + " "
+        ret += time + " "
+        ret += locator + " "
+        return ret
