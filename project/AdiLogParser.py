e900bc507b48362dad2d55c279c1178b20cd0727
szwagier90 <szwagier90@gmail.com>
2015-05-19 00:12:41 +0200

    Basic parsing, still a long way to go

--- project/AdiLogParser.py
+++ project/AdiLogParser.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import re
+
 class AdiLogParser:
     def __init__(self, path):
         self.path = path
@@ -20,4 +22,43 @@ class AdiLogParser:
 
     def parse_one_record(self, record):
         parsed_record = dict()
+        replaced_record = record.replace('>>', '$3e3e').replace('<<', '$3c3c')
+        tags = re.findall('<(.*?):(\d+).*?>([^<\t\n\r\f\v]+ )', replaced_record)
+        for t in tags:
+            key = t[0]
+            corrected_value = t[2].replace('$3e3e', '>>').replace('$3c3c', '<<')
+            value = corrected_value[:int(t[1])]
+
+            if key == 'CQZ':
+                value = int(value)
+            elif key == 'DXCC':
+                value = int(value)
+            elif key == 'ITUZ':
+                value = int(value)
+            elif key == 'APP_LOGGER32_QSL':
+                value = self.ascii_to_bool(value)
+            elif key == 'QSL_SENT':
+                value = self.ascii_to_bool(value)
+            elif key == 'APP_LOGGER32_QSL':
+                value = self.ascii_to_bool(value)
+            elif key == 'eQSL_QSL_SENT':
+                value = self.ascii_to_bool(value)
+            elif key == 'LOTW_QSL_SENT':
+                value = self.ascii_to_bool(value)
+            elif key == "APP_LOGGER32_QSO_NUMBER":
+                value = int(value)
+            elif key == "APP_LOGGER32_LAT":
+                value = float(value)
+            elif key == "APP_LOGGER32_LNG":
+                value = float(value)
+
+            parsed_record[key] = value
+
+        parsed_record['DATE'] = None
         return parsed_record
+
+    def ascii_to_bool(self, value):
+        if value == 'Y':
+            return True
+        elif value == 'N':
+            return False
--- project/test_AdiLogParser.py
+++ project/test_AdiLogParser.py
@@ -2,8 +2,10 @@
 
 import unittest
 from AdiLogParser import AdiLogParser
+from datetime import datetime
+import pytz
 
-test_record = '<BAND:3>80M <CALL:6>SP3CMX <CONT:2>EU <CQZ:2>15 <DXCC:3>269 <ITUZ:2>28 <MODE:3>SSB <OPERATOR:6>SP6QNU <PFX:3>SP3 <APP_LOGGER32_QSL:1>Y <QSL_SENT:1>Y <QSLMSG:11>73 - Mietek <QSO_DATE:8:D>20090915 <TIME_ON:6>165400 <RST_SENT:2>59 <TIME_OFF:6>165400 <eQSL_QSL_SENT:1>Y <LOTW_QSL_SENT:1>Y <APP_LOGGER32_QSO_NUMBER:1>3 <COUNTRY:6>Poland <APP_LOGGER32_LAT:5>52.39 <APP_LOGGER32_LNG:6>-16.84 <EOR>'
+test_record1 = '<BAND:3>80M <CALL:6>SP3CMX <CONT:2>EU <CQZ:2>15 <DXCC:3>269 <ITUZ:2>28 <MODE:3>SSB <OPERATOR:6>SP6QNU <PFX:3>SP3 <APP_LOGGER32_QSL:1>Y <QSL_SENT:1>Y <QSLMSG:11>73 - Mietek <QSO_DATE:8:D>20090915 <TIME_ON:6>165400 <RST_SENT:2>59 <TIME_OFF:6>165400 <eQSL_QSL_SENT:1>Y <LOTW_QSL_SENT:1>Y <APP_LOGGER32_QSO_NUMBER:1>3 <COUNTRY:6>Poland <APP_LOGGER32_LAT:5>52.39 <APP_LOGGER32_LNG:6>-16.84 <EOR>'
 
 class ADItesting(unittest.TestCase):
     def setUp(self):
@@ -14,11 +16,35 @@ class ADItesting(unittest.TestCase):
         self.assertEqual(self.parser.path, "test_log.adi")
 
     def test_get_one_record(self):
-        self.assertEqual(self.parser.records[2], test_record)
+        self.assertEqual(self.parser.records[2], test_record1)
 
-    def test_parse_one_record(self):
-        parsed_record = self.parser.parse_one_record(test_record)
+    def test_parse_first_record(self):
+        parsed_record = self.parser.parse_one_record(test_record1)
         self.assertIsInstance(parsed_record, dict)
+        self.assertTrue("BAND" in parsed_record.keys())
+        self.assertEqual(parsed_record["BAND"], "80M")
+        self.assertEqual(parsed_record["CALL"], "SP3CMX")
+        self.assertEqual(parsed_record["CONT"], "EU")
+        self.assertEqual(parsed_record["CQZ"], 15)
+        self.assertEqual(parsed_record["DXCC"], 269)
+        self.assertEqual(parsed_record["ITUZ"], 28)
+        self.assertEqual(parsed_record["MODE"], "SSB")
+        self.assertEqual(parsed_record["OPERATOR"], "SP6QNU")
+        self.assertEqual(parsed_record["PFX"], "SP3")
+        self.assertEqual(parsed_record["APP_LOGGER32_QSL"], True)
+        self.assertEqual(parsed_record["QSL_SENT"], True)
+        self.assertEqual(parsed_record["QSLMSG"], "73 - Mietek")
+        self.assertEqual(parsed_record["QSO_DATE"], "20090915")
+        self.assertEqual(parsed_record["TIME_ON"], "165400")
+        self.assertEqual(parsed_record["RST_SENT"], "59")
+        self.assertEqual(parsed_record["TIME_OFF"], "165400")
+        self.assertEqual(parsed_record["eQSL_QSL_SENT"], True)
+        self.assertEqual(parsed_record["LOTW_QSL_SENT"], True)
+        self.assertEqual(parsed_record["APP_LOGGER32_QSO_NUMBER"], 3)
+        self.assertEqual(parsed_record["COUNTRY"], "Poland")
+        self.assertEqual(parsed_record["APP_LOGGER32_LAT"], 52.39)
+        self.assertEqual(parsed_record["APP_LOGGER32_LNG"], -16.84)
+        self.assertEqual(parsed_record["DATE"], datetime(2009, 9, 15, 16, 54, 0, tzinfo=pytz.utc))
 
 if __name__ == '__main__':
     unittest.main()
