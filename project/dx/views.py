d234afd648c390e7689a5d47fc939e86a41419a5
szwagier90 <szwagier90@gmail.com>
2015-05-20 00:38:44 +0200

    Show the 10 latest saved Spots on the home page

--- project/dx/views.py
+++ project/dx/views.py
@@ -17,7 +17,7 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['last_five_spots'] = Spot.objects.order_by('time')[:5]
+        context['last_five_spots'] = Spot.objects.order_by('-id')[:10]
         return context
 
 class RegisterView(FormView):
