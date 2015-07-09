e4c68f98042a8e17fbc5be2a7578006ec2954089
szwagier90 <szwagier90@gmail.com>
2015-07-09 03:59:14 +0200

    Added new custom Django Command. manage.py parse_log file_path username

---
+++ project/dx/management/commands/parse_log.py
@@ -0,0 +1,140 @@
+from django.core.management.base import BaseCommand
+
+from AdiLogParser import AdiLogParser
+
+from dx.models import Operator, Prefix, QSO
+
+from django.db import IntegrityError
+from drafts import colors
+
+class LogEntryError(Exception):
+    pass
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        adi = AdiLogParser(args[0])
+
+        operator = Operator.objects.get(user__username=args[1])
+
+        for record in adi.parsed_records:
+            try:
+                prefix = self.get_or_create_prefix(record)
+
+                call = self.read_key_from_record('CALL', record)
+                date = self.read_key_from_record('DATE', record)
+                band = self.read_key_from_record('BAND', record)
+                frequency = self.read_key_from_record('FREQ', record)
+                locator = self.read_key_from_record('GRIDSQUARE', record)
+                mode = self.read_key_from_record('MODE', record)
+                rst_sent = self.read_key_from_record('RST_SENT', record)
+                rst_received = self.read_key_from_record('RST_RCVD', record)
+                qsl_confirmed = self.read_key_from_record('QSL_RCVD', record)
+                eqsl_confirmed = self.read_key_from_record('eQSL_QSL_RCVD', record)
+                lotw_confirmed = self.read_key_from_record('LOTW_QSL_RCVD', record)
+
+                qso, created = QSO.objects.get_or_create(
+                    call=call,
+                    date=date,
+                    band=band,
+                    frequency=frequency,
+                    locator=locator,
+                    mode=mode,
+                    defaults={
+                        'operator': operator,
+                        'prefix': prefix,
+                        'rst_sent': rst_sent,
+                        'rst_received': rst_received,
+                        'qsl_confirmed': qsl_confirmed,
+                        'eqsl_confirmed': eqsl_confirmed,
+                        'lotw_confirmed': lotw_confirmed,
+                    }
+                )
+
+                if not created:
+                    qso.qsl_confirmed = qsl_confirmed
+                    qso.eqsl_confirmed = eqsl_confirmed
+                    qso.lotw_confirmed = lotw_confirmed
+                    qso.save()
+
+            except IntegrityError:
+                print "IntegrityError"
+                continue
+            except LogEntryError:
+                print colors.magenta("LogEntryError!")
+                print colors.cyan(record)
+
+    def get_or_create_prefix(self, record):
+        prefix = None
+
+        callsign = self.read_key_from_record('CALL', record)
+        try:
+            prefix = Prefix.objects.get(full_callsign=True, name=callsign)
+        except Prefix.DoesNotExist:
+            prefix = self.match_prefix(record)
+        return prefix
+
+    def match_prefix(self, record):
+        prefix = None
+
+        pfx = self.read_key_from_record('PFX', record)
+
+        for i in range(len(pfx)):
+            try:
+                prefix = Prefix.objects.get(name=pfx[:len(pfx)-i])
+            except Prefix.DoesNotExist:
+                continue
+            else:
+                break
+        else:
+            prefix = self.create_prefix(record)
+        return prefix
+
+    def create_prefix(self, record):
+        dxcc = self.read_key_from_record('DXCC', record)
+        name = self.read_key_from_record('COUNTRY', record)
+        entity, created = Entity.objects.get_or_create(
+            id=dxcc,
+            defaults={
+                'id': dxcc, 
+                'name': name
+            }
+        )
+
+        pfx = self.read_key_from_record('PFX', record)
+        cqz = self.read_key_from_record('CQZ', record)
+        ituz = self.read_key_from_record('ITUZ', record)
+        prefix = Prefix.objects.create(
+            entity=entity,
+            name=pfx,
+            ituz=ituz,
+            cqz=cqz,
+            full_callsign=False
+        )
+
+        return prefix
+
+    def read_key_from_record(self, key, record):
+        exception_values = {
+            'CALL': None,
+            'DATE': None,
+            'COUNTRY': None,
+            'CQZ': None,
+            'BAND': None,
+            'ITUZ': None,
+            'FREQ': None,
+            'GRIDSQUARE': None,
+            'MODE': None,
+            'RST_SENT': None,
+            'RST_RCVD': None,
+            'QSL_RCVD': False,
+            'eQSL_QSL_RCVD': False,
+            'LOTW_QSL_RCVD': False,
+        }
+
+        try:
+            return record[key]
+        except KeyError:
+            try:
+                return exception_values[key]
+            except KeyError:
+                raise LogEntryError
--- project/dx/views.py
+++ project/dx/views.py
@@ -16,10 +16,8 @@ from forms import LogUploadForm, ProfileForm
 
 from tempfile import NamedTemporaryFile
 
-from AdiLogParser import AdiLogParser
-
-from drafts import colors
-from django.db import IntegrityError
+import sys
+import subprocess
 
 class IndexView(TemplateView):
     template_name = 'dx/index.html'
@@ -90,126 +88,15 @@ class LogUploadView(FormView):
         return super(LogUploadView, self).form_valid(form)
 
     def handle_uploaded_file(self, f):
-        with NamedTemporaryFile() as temp_file:
+        temp_file = NamedTemporaryFile(delete=False)
+
+        with temp_file as t_f:
             for chunk in f.chunks():
-                temp_file.write(chunk)
-            temp_file.flush()
-            adi = AdiLogParser(temp_file.name)
+                t_f.write(chunk)
+            t_f.flush()
 
-        operator = Operator.objects.get(user__username=self.request.user)
-
-        for record in adi.parsed_records:
-            try:
-                prefix = self.get_or_create_prefix(record)
-
-                call = self.read_key_from_record('CALL', record)
-                date = self.read_key_from_record('DATE', record)
-                band = self.read_key_from_record('BAND', record)
-                frequency = self.read_key_from_record('FREQ', record)
-                locator = self.read_key_from_record('GRIDSQUARE', record)
-                mode = self.read_key_from_record('MODE', record)
-                rst_sent = self.read_key_from_record('RST_SENT', record)
-                rst_received = self.read_key_from_record('RST_RCVD', record)
-                qsl_confirmed = self.read_key_from_record('QSL_RCVD', record)
-                eqsl_confirmed = self.read_key_from_record('eQSL_QSL_RCVD', record)
-                lotw_confirmed = self.read_key_from_record('LOTW_QSL_RCVD', record)
-
-                qso, created = QSO.objects.get_or_create(
-                    call=call,
-                    date=date,
-                    band=band,
-                    frequency=frequency,
-                    locator=locator,
-                    mode=mode,
-                    defaults={
-                        'operator': operator,
-                        'prefix': prefix,
-                        'rst_sent': rst_sent,
-                        'rst_received': rst_received,
-                        'qsl_confirmed': qsl_confirmed,
-                        'eqsl_confirmed': eqsl_confirmed,
-                        'lotw_confirmed': lotw_confirmed,
-                    }
-                )
-                print i, created, qso
-
-                if not created:
-                    qso.qsl_confirmed = qsl_confirmed
-                    qso.eqsl_confirmed = eqsl_confirmed
-                    qso.lotw_confirmed = lotw_confirmed
-                    qso.save()
-
-            except IntegrityError:
-                print "IntegrityError"
-                continue
-
-    def get_or_create_prefix(self, record):
-        prefix = None
-
-        callsign = self.read_key_from_record('CALL', record)
-        try:
-            prefix = Prefix.objects.get(full_callsign=True, name=callsign)
-        except Prefix.DoesNotExist:
-            prefix = self.match_prefix(record)
-        return prefix
-
-    def match_prefix(self, record):
-        prefix = None
-
-        pfx = self.read_key_from_record('PFX', record)
-
-        for i in range(len(pfx)):
-            try:
-                prefix = Prefix.objects.get(name=pfx[:len(pfx)-i])
-            except Prefix.DoesNotExist:
-                continue
-            else:
-                break
-        else:
-            prefix = self.create_prefix(record)
-        return prefix
-
-    def create_prefix(self, record):
-        dxcc = self.read_key_from_record('DXCC', record)
-        name = self.read_key_from_record('COUNTRY', record)
-        entity, created = Entity.objects.get_or_create(
-            id=dxcc,
-            defaults={'name': name}
+        subprocess.Popen(
+            [sys.executable, 'manage.py', 'parse_log', temp_file.name, str(self.request.user)],
+            # stdout=subprocess.PIPE,
+            # stderr=subprocess.STDOUT,
         )
-
-        pfx = self.read_key_from_record('PFX', record)
-        cqz = self.read_key_from_record('CQZ', record)
-        ituz = self.read_key_from_record('ITUZ', record)
-        prefix = Prefix.objects.create(
-            entity=entity,
-            name=pfx,
-            ituz=ituz,
-            cqz=cqz,
-            full_callsign=False
-        )
-
-        return prefix
-
-    def read_key_from_record(self, key, record):
-        exception_values = {
-            'CALL': None,
-            'DATE': None,
-            'COUNTRY': None,
-            'CQZ': None,
-            'BAND': None,
-            'ITUZ': None,
-            'DXCC': None,
-            'FREQ': None,
-            'GRIDSQUARE': None,
-            'MODE': None,
-            'RST_SENT': None,
-            'RST_RCVD': None,
-            'QSL_RCVD': False,
-            'eQSL_QSL_RCVD': False,
-            'LOTW_QSL_RCVD': False,
-        }
-
-        try:
-            return record[key]
-        except KeyError:
-            return exception_values[key]
